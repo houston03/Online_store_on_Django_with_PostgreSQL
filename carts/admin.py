431bf7e4aaf895c55f77bb3ad4a1519403541e84
Pavel Khmel <152857475+houston03@users.noreply.github.com>
2024-11-02 09:58:49 +0300

    admin.py

--- carts/admin.py
+++ carts/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from carts.models import Cart
 
 
-# admin.site.register(Cart)
+
 class CartTabAdmin(admin.TabularInline):
     model = Cart
     fields = "product", "quantity", "created_timestamp"
@@ -25,6 +25,6 @@ class CartAdmin(admin.ModelAdmin):
     def product_display(self, obj):
         return str(obj.product.name)
 
-    # user_display and product_display alter name of columns in admin panel
+
     user_display.short_description = "Пользователь"
     product_display.short_description = "Товар"
