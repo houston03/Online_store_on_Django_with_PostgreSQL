814b883602bf2aff38fa7630208957279bae441a
Pavel Khmel <152857475+houston03@users.noreply.github.com>
2024-11-04 13:37:39 +0300

    admin.py

--- goods/admin.py
+++ goods/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 from goods.models import Categories, Products
 
-# admin.site.register(Categories)
-# admin.site.register(Products)
+
 
 
 @admin.register(Categories)
